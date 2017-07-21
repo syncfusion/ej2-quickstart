352220bbd4c0cee291c1d5752fb7962b73e6c228
Sridhar S <sridhars@syncfusion.com>
2017-07-21 16:04:06 +0530

    Committing for empty button initialized content

--- src/app/app.ts
+++
@@ -1,7 +0,0 @@
-import { Button } from '@syncfusion/ej2-buttons';
-
-// initialize button component 
-let button: Button = new Button();
-
-// render initialized button
-button.appendTo('#normalbtn');
