38198a1ac5de580b7cd22dfaba96e578b2a9c772
Prabu Sarvesan <prabhusarvesan@syncfusion.com>
2019-10-07 16:10:46 +0530

    Added Spreadsheet reference

--- src/system.config.js
+++ src/system.config.js
@@ -20,7 +20,8 @@ System.config({
         "@syncfusion/ej2-calendars": "syncfusion:ej2-calendars/dist/ej2-calendars.umd.min.js",
         "@syncfusion/ej2-grids": "syncfusion:ej2-grids/dist/ej2-grids.umd.min.js",
         "@syncfusion/ej2-dropdowns": "syncfusion:ej2-dropdowns/dist/ej2-dropdowns.umd.min.js",
-        "@syncfusion/ej2-querybuilder": "syncfusion:ej2-querybuilder/dist/ej2-querybuilder.umd.min.js"
+        "@syncfusion/ej2-querybuilder": "syncfusion:ej2-querybuilder/dist/ej2-querybuilder.umd.min.js",
+        "@syncfusion/ej2-spreadsheet": "syncfusion:ej2-spreadsheet/dist/ej2-spreadsheet.umd.min.js"
     },
     packages: {
         'app': { main: 'app', defaultExtension: 'js' }
